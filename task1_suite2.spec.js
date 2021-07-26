e51b7a0ec6f2a366cf667bc2196ba89c878216be
Ayat-Ayyad <74051755+Ayat-Ayyad@users.noreply.github.com>
2021-07-26 17:21:30 +0300

    Update task1_suite2.spec.js
    updated selectors

--- task1_suite2.spec.js
+++ task1_suite2.spec.js
@@ -1,27 +1,24 @@
-
-
 describe('Test Suite 2', () => {
+
     beforeEach(() => {
         cy.visit('https://www.seleniumeasy.com/test/table-search-filter-demo.html');
     });
+
     it('Test Case 1: To search for an entry in the table. ', () => {
-
         cy.get('#task-table-filter').type('Bug');
-
-        cy.get('tbody > :nth-child(7) > :nth-child(2)').should('contain', 'Bug')
+        cy.get('td').should('contain', 'Bug')
     });
+
     it('Test Case 2:o check that filters are disabled before clicking on filter icon, then filter by username', () => {
         
-        cy.get(':nth-child(1) > .form-control').should('be.disabled');
-        cy.get('.filters > :nth-child(2) > .form-control').should('be.disabled');
-        cy.get(':nth-child(3) > .form-control').should('be.disabled');
-        cy.get(':nth-child(4) > .form-control').should('be.disabled');
+        cy.get('.form-control').should('be.disabled');
+     
+        cy.get('.pull-right > button').click();
+        cy.get('.filters >  :nth-child(2)').type('jacobs');
 
-        cy.get('body > div.container-fluid.text-center > div > div.col-md-6.text-left > div:nth-child(3) > div > div > div > button > span').click();
-        cy.get('body > div.container-fluid.text-center > div > div.col-md-6.text-left > div:nth-child(3) > div > table > thead > tr > th:nth-child(2) > input').type('jacobs');
-
-        cy.get(':nth-child(3) > .panel > .table > tbody > :nth-child(2) > :nth-child(2)').should('contain', 'jacobs')
+        cy.get('td').should('contain', 'jacobs')
 
     });
 });
+
 
